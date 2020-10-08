d83592c724b9f44af3673dbb67ef37fba7908a20
Timothy Lyons <t.brandon.lyons@gmail.com>
2020-10-08 20:55:16 +0000

    Implement std::remove_cv

--- include/vodka/array/to_array.hpp
+++ include/vodka/array/to_array.hpp
@@ -3,9 +3,10 @@
 #ifndef TYBL_VODKA_ARRAY_TOARRAY_HPP
 #define TYBL_VODKA_ARRAY_TOARRAY_HPP
 
+#include "vodka/type_traits/remove_cv.hpp"
 #include "vodka/utility/move.hpp"
 
-#include <type_traits> // std::remove_cv_t, std::is_*_constructable_v, std::is_array
+#include <type_traits> // std::is_*_constructable_v, std::is_array
 #include <utility>     // std::index_sequence, std::make_index_sequence
 
 namespace tybl::vodka::array {
@@ -14,13 +15,13 @@ namespace tybl::vodka::array {
 
 template <typename T, size_t N, size_t... I>
 constexpr auto
-impl_lvalue_to_array(T (&a)[N], std::index_sequence<I...> /*indices*/) -> array<std::remove_cv_t<T>, N> {
+impl_lvalue_to_array(T (&a)[N], std::index_sequence<I...> /*indices*/) -> array<type_traits::remove_cv_t<T>, N> {
   return {{a[I]...}};
 }
 
 template <typename T, size_t N>
 constexpr auto
-to_array(T (&a)[N]) noexcept(std::is_nothrow_constructible_v<T, T&>) -> array<std::remove_cv_t<T>, N> {
+to_array(T (&a)[N]) noexcept(std::is_nothrow_constructible_v<T, T&>) -> array<type_traits::remove_cv_t<T>, N> {
   static_assert(!std::is_array_v<T>, "[array.creation]/1: to_array does not accept multidimensional arrays.");
   static_assert(std::is_constructible_v<T, T&>, "[array.creation]/1: to_array requires copy constructible elements.");
   return impl_lvalue_to_array(a, std::make_index_sequence<N>());
@@ -28,13 +29,13 @@ to_array(T (&a)[N]) noexcept(std::is_nothrow_constructible_v<T, T&>) -> array<st
 
 template <typename T, size_t N, size_t... I>
 constexpr auto
-impl_rvalue_to_array(T(&&a)[N], std::index_sequence<I...> /*indices*/) -> array<std::remove_cv_t<T>, N> {
+impl_rvalue_to_array(T(&&a)[N], std::index_sequence<I...> /*indices*/) -> array<type_traits::remove_cv_t<T>, N> {
   return {{utility::move(a[I])...}};
 }
 
 template <typename T, size_t N>
 constexpr auto
-to_array(T(&&a)[N]) noexcept(std::is_nothrow_move_constructible_v<T>) -> array<std::remove_cv_t<T>, N> {
+to_array(T(&&a)[N]) noexcept(std::is_nothrow_move_constructible_v<T>) -> array<type_traits::remove_cv_t<T>, N> {
   static_assert(!std::is_array_v<T>, "[array.creation]/4: to_array does not accept multidimensional arrays.");
   static_assert(std::is_move_constructible_v<T>, "[array.creation]/4: to_array requires move constructible elements.");
   return impl_rvalue_to_array(utility::move(a), std::make_index_sequence<N>());
---
+++ include/vodka/type_traits/remove_cv.hpp
@@ -0,0 +1,33 @@
+// License: The Unlicense (https://unlicense.org)
+#pragma once
+#ifndef TYBL_VODKA_TYPETRAITS_REMOVECV_HPP
+#define TYBL_VODKA_TYPETRAITS_REMOVECV_HPP
+
+namespace tybl::vodka::type_traits {
+
+template <class T>
+struct remove_cv {
+  using type = T;
+};
+
+template <class T>
+struct remove_cv<const T> {
+  using type = T;
+};
+
+template <class T>
+struct remove_cv<volatile T> {
+  using type = T;
+};
+
+template <class T>
+struct remove_cv<const volatile T> {
+  using type = T;
+};
+
+template <class T>
+using remove_cv_t = typename remove_cv<T>::type;
+
+} // namespace tybl::vodka::type_traits
+
+#endif // TYBL_VODKA_TYPETRAITS_REMOVECV_HPP
